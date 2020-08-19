24f5c56e4e86add8e1dbbb7936c54a94987c33e5
Bowser65 <bowoser@weeb.services>
2020-08-19 03:31:27 +0200

    Better compatibility with themes

--- index.js
+++ index.js
@@ -11,19 +11,20 @@ const { React, getModule } = require('powercord/webpack');
 module.exports = class QuickStar extends Plugin {
   async startPlugin () {
     const classes = {
-      ...await getModule([ 'emojiButton' ]),
-      ...await getModule([ 'icon', 'isHeader' ])
+      ...await getModule([ 'icon', 'isHeader' ]),
+      ...await getModule([ 'button', 'separator', 'wrapper' ])
     };
     const reactionManager = await getModule([ 'addReaction' ]);
     const MiniPopover = await getModule(m => m.default && m.default.displayName === 'MiniPopover');
-    inject('star-button', MiniPopover, 'default', (args, res) => {
+    inject('star-button', MiniPopover, 'default', (_, res) => {
       const props = findInReactTree(res, r => r && r.canReact && r.message);
-      if (!props || props.message.reactions.find(r => r.emoji.name === '⭐' && r.me)) return res;
+      if (!props || props.message.reactions.find(r => r.emoji.name === '⭐' && r.me)) {
+        return res;
+      }
 
       res.props.children.unshift(React.createElement(
         'div', {
-          className: `${classes.emojiButton} ${classes.button}`,
-          style: { cursor: 'pointer' },
+          className: classes.button,
           onClick: () => reactionManager.addReaction(props.channel.id, props.message.id, {
             animated: false,
             name: '⭐',
